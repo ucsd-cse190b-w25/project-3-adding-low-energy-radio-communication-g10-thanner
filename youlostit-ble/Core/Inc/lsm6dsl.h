d48b14acc1c02aba4cb9b8586df3ebf30b434904
Thanh2x <tvhoang@ucsd.edu>
2025-03-14 01:41:50 -0700

    COMMIT? MORE LIKE COMMIT SUICIDE! HAHA

--- youlostit-ble/Core/Inc/lsm6dsl.h
+++ youlostit-ble/Core/Inc/lsm6dsl.h
@@ -10,5 +10,7 @@
 
 void lsm6dsl_init();
 void lsm6dsl_read_xyz(int16_t* x, int16_t* y, int16_t* z);
+void lsm6dsl_init_movement_detection();
+uint8_t lsm6dsl_check_movement();
 
 #endif /* LSM6DSL_H_ */
